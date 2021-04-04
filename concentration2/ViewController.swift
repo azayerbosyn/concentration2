35e446621b62acc5372f261ce7c4ed49e771807f
Azamat Erbosyn <Erbosyn75@gmail.com>
2021-04-04 22:30:50 +0600

    Initial Commit

---
+++ concentration2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  concentration2
+//
+//  Created by Азамат on 04.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
